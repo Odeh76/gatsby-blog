7ce70c7398d547cbb500f87f74fd06fa5b6a774f
Odeh76 <aodeh76@gmail.com>
2023-12-23 10:44:51 -0800

    content edits and minor styling tweaks and adding global blockquote styles

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,8 +5,8 @@ function Footer() {
   return (
     <footer className='footer'>
       <ul className='flex wrap'>
-        <li><a className='link mr-1' href='' target="_blank">LinkedIn</a></li>
-        <li><a className='link mr-1' href='' target="_blank">GitHub</a></li>
+        <li><a className='link mr-1' href='https://www.linkedin.com/in/abdullah-odeh' target="_blank">LinkedIn</a></li>
+        <li><a className='link mr-1' href='https://www.instagram.com/a_odeh76' target="_blank">Instagram</a></li>
       </ul>
     </footer>
   )
