6d26d3e2cfc057dc6b937de7f7c84dff569c88ae
Odeh76 <aodeh76@gmail.com>
2023-12-25 10:06:29 -0800

    minor content and styling changes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,11 +7,11 @@ function Header() {
     <nav className='nav'>
       <ul className='flex justify-between align-center'>
         <div>
-          <li><Link className='link' to='/'>Home</Link></li>
+          <li><Link className='nav-link' to='/'>Home</Link></li>
         </div>
         <div className='flex'>
-          <li><Link className='link mr-1' to='/about'>About</Link></li>
-          <li><Link className='link' to='/blog'>Blog</Link></li>
+          <li><Link className='nav-link mr-1' to='/about'>About</Link></li>
+          <li><Link className='nav-link' to='/blog'>Blog</Link></li>
         </div>
       </ul>
     </nav>
