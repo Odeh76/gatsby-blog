4325cbf8f99550c8777426d08627c38a0ca2aeb1
Odeh76 <aodeh76@gmail.com>
2024-11-01 15:32:09 -0700

    Revert "removes personal writing content, updates image background"
    This reverts commit 1a377ca51c7cc383cef01bc16b45c11a317f3773.

---
+++ src/pages/blog/random.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+function Random() {
+  return (
+    <Layout>Random</Layout>
+  )
+}
+
+export default Random
+
+export const Head = () => <title>Random | Abdullah Odeh</title>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,34 +1,13 @@
 import * as React from "react"
 import Layout from "../components/Layout"
-import { StaticImage } from 'gatsby-plugin-image';
-
 const Home = () => {
   return (
-    <>
-       <StaticImage
-      src="../images/optimized_image.png" // Replace with your image path
-      alt="Description of image"
-      placeholder="blurred" // Optional: "tracedSVG", "dominantColor", "none"
-      layout="constrained" // Optional: "fixed", "fullWidth"
-      style={{
-        position: 'absolute',
-        zIndex: '-1',
-        width: '100%',
-        height: '100%',
-        objectFit: 'cover',         // Ensures the image covers the container
-        objectPosition: 'center',   // Centers the image within the container
-        borderRadius: '8px',        // Adds subtle rounded corners (optional)
-        boxShadow: '0 4px 12px rgba(0, 0, 0, 0.2)', // Soft shadow for depth
-        transition: 'transform 0.3s ease-in-out',   // Smooth zoom effect (optional)
-      }}
-      />
     <Layout>
         <h5 className="mb-3">Hello! My name is</h5>
         <h1 className="mb-3 bold uppercase">Abdullah Odeh.</h1>
         <h5 className="mb-3">I'm a Web Developer based in Los Angeles, CA.</h5>
         <a href="mailto:abdullahsayshello@gmail.com" target="_blank" className="main-btn">Connect</a>
     </Layout>
-      </>
   )
 }
 
