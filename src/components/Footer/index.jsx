38da050efe797d84c61131ceb82ba0ec6f0236f9
Odeh76 <aodeh76@gmail.com>
2024-11-12 09:23:08 -0800

    adds instagram link and rel noreferrer noopener to anchor tags

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <footer className='footer'>
       <ul className='flex wrap'>
         <li><a className='nav-link mr-1' rel="noreferrer" href='https://www.linkedin.com/in/abdullah-odeh' target="_blank">LinkedIn</a></li>
-        {/* <li><a className='nav-link mr-1' rel="noreferrer" href='https://www.instagram.com/a_odeh76' target="_blank">Instagram</a></li> */}
+        <li><a className='nav-link mr-1' rel="noreferrer" href='https://www.instagram.com/a_odeh76' target="_blank">Instagram</a></li>
       </ul>
     </footer>
   )
